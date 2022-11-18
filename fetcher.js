bbb6e6a8472ccbe0712e793d416eebf07ca05863
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-11-18 14:52:20 -0800

    adding assignment code and stretch 1

--- fetcher.js
+++ fetcher.js
@@ -1,33 +1,53 @@
 const fs = require('fs');
 const request = require('request');
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const args = process.argv;
 let argRequests = args.slice(2);
-let input = String(argRequests[0]);
-let file = String(argRequests[1]);
-let content;
+let input = argRequests[0];
+let file = argRequests[1];
 
-console.log(input, file);
+// request the file from the server
 
  request(input, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  content = body;
+
+  // if file exists
+  fs.access(file, fs.F_OK, (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  
+    //file exists
+    rl.question('File exists already, press Y to overwrite it and N to exit ', (answer) => {
+      if (answer === 'y') {
+        fs.writeFile(file, body, err => {
+          if (err) {
+            console.error(err);
+          }
+          // file written successfully
+          console.log(`Downloaded and saved ${body.length} bytes to ${file}`)
+          rl.close();
+
+        });
+      }
+      if (answer === 'n') {
+      rl.close();
+      }
+    // console.log('file exists')
+  })
+
+  // fs.writeFile(file, body, err => {
+  //   if (err) {
+  //     console.error(err);
+  //   }
+  //   // file written successfully
+  //   console.log(`Downloaded and saved ${body.length} bytes to ${file}`)
+  });
 });
-
-
-fs.writeFile(file, content, err => {
-  if (err) {
-    console.error(err);
-  }
-  // file written successfully
-});
-
-
-// fsPromises.writeFile('notes.txt', 'Hey, we changed it!\r\n', {
-//   encoding: 'utf-8', flag: 'w'
-// })
-// .then(() => {
-//   console.log('File write complete!');
-// });
